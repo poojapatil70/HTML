a2259dfc7a74a2ab174428bc1ce66a2f48c6f816
poojapatil70 <poojapatil775677@gmail.com>
2026-03-20 06:55:28 -0700

    Add files via upload

---
+++ JAVA_Class/class.java
@@ -0,0 +1,11 @@
+public class class {
+
+    public static void main(String[] args) {
+        //output
+        System.out.println("HELLOW JAVA class");
+        System.out.println("My name os pooja");
+
+        int number = 10
+
+    }
+}
