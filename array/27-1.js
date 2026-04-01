9609d9656d6193f1ab31bb3d1fc8a8a2dd3dd90b
poojapatil70 <poojapatil775677@gmail.com>
2026-04-01 17:54:10 +0530

    Add files via upload

---
+++ array/27-1.js
@@ -0,0 +1,14 @@
+//create array
+let number = [10,20,30,40]
+//length: 1,2,3,4
+
+//index: 0,1,2,3
+
+let strings = ["string1" , "string"]
+let answers = (true,false)
+//array is collection of similar data type
+//let array= ["string",10]
+//array read
+console.log(number)
+//console.log(numbers[10])
+//console.log(typeof(array[1]))
