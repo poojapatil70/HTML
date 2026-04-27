46a259e32f24df68bf751583f7c813a8e17cfe9e
poojapatil70 <poojapatil775677@gmail.com>
2026-04-27 20:00:34 +0530

    Add files via upload

---
+++ JAVA_Class/classOne.java
@@ -0,0 +1,33 @@
+public class classOne{
+    public static void main (String[] args){
+        System.out.println("Welcome to JAVA");
+
+        //Integer
+        int number = 10; //capacity 10 digits
+        System.out.println(number);
+
+        short num = 11111;  //capacity 5 digits
+        System.out.println(num);
+
+        byte n = 42; //three number capacity
+        System.out.println(n);
+
+        long longNumber = 7972677714775607513l;//cpacity is 10 but after applying l at last total 20 number we can add
+        System.out.println(longNumber);
+
+        short shortNumber = 7777; //capacity of 4 number
+        System.out.println(shortNumber);
+
+        char grade = 'A';//only one leter capacity
+        System.out.println(grade);
+
+        boolean isActive = false; //only true false
+        System.out.println(isActive);
+
+        double price = 19.99678645678768678; //capacity of unlimited number in point only and same price will show
+        System.out.println(price);
+
+        float height = 16.35454545454512f; //capacity is unlimited in point number but f is compulsary and point number is show lessbut in double point number will show unlimited
+        System.out.println(height);
+    }
+}
